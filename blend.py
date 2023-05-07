0950b49bf395099bbf251531c973f845edd8dd66
KC <13112812258@163.com>
2023-05-07 23:22:28 +0800

    遮罩混合处理

--- blend.py
+++ blend.py
@@ -1,7 +1,14 @@
 from PIL import Image
-img1 = Image.open('kc1.jpg').convert(mode="RGB")
-img2 = Image.new("RGB", img1.size, 'blue')
+# img1 = Image.open('kc1.jpg').convert(mode="RGB")
+# img2 = Image.new("RGB", img1.size, 'blue')
 
 
-##混合两幅图
-Image.blend(img1, img2, alpha=0.5).show()
+# 混合两幅图
+# Image.blend(img1, img2, alpha=0.5).show()
+
+# 遮罩混合处理
+img1 = Image.open('kc1.jpg')
+img2 = Image.open('kc2.jpg')
+img2 = img2.resize(img1.size)
+r, g, b = img2.split()
+Image.composite(img1, img2, b).show()
