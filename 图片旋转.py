c10c8be97ff3d52b0ff36fdf42d57fdc3466e754
KC <13112812258@163.com>
2023-05-08 09:58:28 +0800

    图片旋转和格式转换

--- 图片旋转.py
+++ 图片旋转.py
@@ -2,4 +2,12 @@ from PIL import Image
 
 img = Image.open("./kc1.jpg")
 # 图像的旋转
-img.rotate(180).show()
+# img.rotate(180).show()
+
+# 格式抓换
+# img.transpose(Image.FLIP_TOP_BOTTOM).show() # 上下滤镜
+# img.transpose(Image.FLIP_LEFT_RIGHT).show()  # 左右滤镜
+# img.transpose(Image.ROTATE_90).show()   # 90度滤镜
+# img.transpose(Image.TRANSPOSE).show()
+
+
